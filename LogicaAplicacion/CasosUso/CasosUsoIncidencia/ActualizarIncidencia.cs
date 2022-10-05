6d27f300e10d53d5a4488e66d60b20b80882171a
ALVARO REVETRIA <ALVARO_REVETRIA@hotmail.com>
2022-10-05 00:29:03 -0300

    CasosUsoIncidenciasCompleto

---
+++ LogicaAplicacion/CasosUso/CasosUsoIncidencia/ActualizarIncidencia.cs
@@ -0,0 +1,31 @@
+﻿using LogicaAplicacion.InterfacesCasosUso.ICasosUsoIncidencia;
+using LogicaNegocio.Dominio;
+using LogicaNegocio.InterfacesRepositorios;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LogicaAplicacion.CasosUso.CasosUsoIncidencia
+{
+    public class ActualizarIncidencia : IActualizarIncidencia
+    {
+        public IRepositorio<Incidencia> RepoIncidencias { get; set; }
+
+        public ActualizarIncidencia(IRepositorio<Incidencia> repoIncidencia)
+        {
+            RepoIncidencias = repoIncidencia;
+        }
+        public void Update(Incidencia obj)
+        {
+
+            try
+            {
+                RepoIncidencias.Update(obj);
+            }
+            catch (Exception e)
+            {
+                throw new Exception("No se pudo actualizar la Incidencia", e);
+            }
+        } 
+    }
+}
---
+++ LogicaAplicacion/CasosUso/CasosUsoIncidencia/ObtenerIncidencias.cs
@@ -0,0 +1,30 @@
+﻿using LogicaAplicacion.InterfacesCasosUso.ICasosUsoIncidencia;
+using LogicaNegocio.Dominio;
+using LogicaNegocio.InterfacesRepositorios;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LogicaAplicacion.CasosUso.CasosUsoIncidencia
+{
+    public class ObtenerIncidencias : IObtenerIncidencias
+    {
+        public IRepositorio<Incidencia> RepoIncidencias { get; set; }
+
+        public ObtenerIncidencias(IRepositorio<Incidencia> repoIncidencia)
+        {
+            RepoIncidencias = repoIncidencia;
+        }
+        public void FindAll(Incidencia obj)
+        {
+            try
+            {
+                RepoIncidencias.FindAll();
+            }
+            catch (Exception e)
+            {
+                throw new Exception("No se pudieron obtener las incidencias", e);
+            }
+        }
+    }
+}
