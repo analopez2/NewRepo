d9e842b62455356519d4cc3274f6bb21a2a734c1
analopez2 <anaplopez07@gmail.com>
2022-11-17 21:17:24 -0300

    actualizacion goles

--- LogicaAccesoDatos/Migrations/20221118000001_goles.Designer.cs
+++ LogicaAccesoDatos/Migrations/20221118000001_goles.Designer.cs
@@ -4,14 +4,16 @@ using LogicaAccesoDatos.BaseDatos;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace LogicaAccesoDatos.Migrations
 {
     [DbContext(typeof(LibreriaContext))]
-    partial class LibreriaContextModelSnapshot : ModelSnapshot
+    [Migration("20221118000001_goles")]
+    partial class goles
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
